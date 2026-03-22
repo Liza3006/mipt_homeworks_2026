458502b4958c32686b4c698fdb85263370eeee28
Орлова Елизавета Константиновна <eliseagle07@gmail.com>
2026-03-23 01:55:29 +0300

    hw

--- part3_types_conditions_loops_functions/hw3.py
+++ part3_types_conditions_loops_functions/hw3.py
@@ -132,6 +132,19 @@ def cost_categories_handler() -> str:
     return "\n".join(lines)
 
 
+
+
+    for transaction in financial_transactions_storage:
+        if not transaction:
+            continue
+        _, month, year = transaction["date"]
+        if transaction["type"] != "income":
+            continue
+        if year == target_year and month == target_month:
+            month_income += transaction["amount"]
+    return month_income
+
+
 def _is_before_date(transaction_date: tuple[int, int, int], query_date: tuple[int, int, int]) -> bool:
     if transaction_date[2] != query_date[2]:
         return transaction_date[2] < query_date[2]
@@ -156,10 +169,12 @@ def _calculate_capital(query_date: tuple[int, int, int]) -> float:
     return capital
 
 
+def _is_same_month(year: int, month: int, query_date: tuple[int, int, int]) -> bool:
+    return year == query_date[2] and month == query_date[1]
+
+
 def _calculate_month_income(query_date: tuple[int, int, int]) -> float:
     month_income = 0
-    target_year = query_date[2]
-    target_month = query_date[1]
 
     for transaction in financial_transactions_storage:
         if not transaction:
@@ -167,7 +182,7 @@ def _calculate_month_income(query_date: tuple[int, int, int]) -> float:
         _, month, year = transaction["date"]
         if transaction["type"] != "income":
             continue
-        if year == target_year and month == target_month:
+        if _is_same_month(year, month, query_date):
             month_income += transaction["amount"]
     return month_income
 
